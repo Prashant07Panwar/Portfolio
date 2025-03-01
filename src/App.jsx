2e642e9fa18c9902f689c9fceeaaf6ac4c919743
Prashant07Panwar <prashantpanwar584@gmail.com>
2025-03-01 13:43:44 +0530

    30/01/2024

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Experience from "./components/Expierence";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import SocialLinks from "./components/SocialLinks";
@@ -22,6 +23,7 @@ const App = () => {
             <Hero />
             <About />
             <Technologies />
+            <Experience />
             <Projects />
             <Contact />
             <SocialLinks />
--- src/constants/index.js
+++ src/constants/index.js
@@ -7,7 +7,7 @@ import project4 from "../assets/projects/project4.jpg";
 export const HERO_CONTENT = `I’m a passionate Full Stack Developer skilled in building dynamic and efficient web applications. From crafting responsive frontends to optimizing backend performance, I enjoy solving complex challenges and turning ideas into impactful solutions. Let’s connect and collaborate!`;
 
 export const ABOUT_TEXT = [
-  `I’m a passionate Full Stack Developer. I completed my graduation in Information Technology with an overall CGPA of 9.02.
+  `I’m a passionate Full Stack Developer. I completed my graduation in Information Technology.
 I specialize in building dynamic and efficient web applications, excelling in both frontend and backend development. I enjoy solving complex challenges, creating responsive designs, and delivering impactful digital solutions.`
 ];
 
@@ -20,41 +20,36 @@ export const DATA = [
   { title: "Contact", href: "#contact" },
 ];
 
-export const EXPERIENCES = [
+export const EXPIERENCE = [
   {
-    year: "June 2023 - July 2024",
-    role: "Web Developer and Research",
-    company: " Bharat Electronics Limited, Ghaziabad, Uttar Pradesh, India",
+    year: "March 2025 - April 2025",
+    role: "ReactJS Developer Intern",
+    company: "Indolike",
     description: [
-      `Developed a dummy website for Bharat Electronic Limited using HTML, CSS, and JavaScript, detailing nine
- locations and showcasing company information.`,
-      `Demonstrated expertise in Software Development Life Cycle for Network-Centric Systems, optimizing
- methodologies and implementing best practices. Researched Radar Systems, including NFTR testing(100% indoor
- testing range), contributing to strategic insights.`,
-    ],
-    technologies: ["Javascript", "HTML", "CSS", "React.js"],
-  },
-  {
-    year: "Jan, 2024 - July, 2024",
-    role: "Full Stack Intern",
-    company:
-      "Cehpoint E-Learning And Cyber Security, Kolkata, West Bengal, India",
-    description: [
-      `Boosted user engagement by 25% with an interactive website, fostering efficient hiring practices for developers and
- employers while providing a user-friendly environment for project execution.`,
-      `Orchestrated the development of an interactive freelancer platform, integrating cutting-edge technologies like
- React.js and Tailwind CSS. Crafted an engaging assessment page with user-centric design, enhancing the hiring
- process and user experience.`,
-    ],
-    technologies: [
-      "Javascript",
-      "HTML",
-      "CSS",
-      "React.js",
-      "Node.js",
-      "MongoDB",
-    ],
-  },
+      `Currently Working.`,
+    ]
+  }
+//   {
+//     year: "Jan, 2024 - July, 2024",
+//     role: "Full Stack Intern",
+//     company:
+//       "Cehpoint E-Learning And Cyber Security, Kolkata, West Bengal, India",
+//     description: [
+//       `Boosted user engagement by 25% with an interactive website, fostering efficient hiring practices for developers and
+//  employers while providing a user-friendly environment for project execution.`,
+//       `Orchestrated the development of an interactive freelancer platform, integrating cutting-edge technologies like
+//  React.js and Tailwind CSS. Crafted an engaging assessment page with user-centric design, enhancing the hiring
+//  process and user experience.`,
+//     ],
+//     technologies: [
+//       "Javascript",
+//       "HTML",
+//       "CSS",
+//       "React.js",
+//       "Node.js",
+//       "MongoDB",
+//     ],
+//   },
 ];
 
 export const PROJECTS = [
